aee03da9e3e4cbd7579996be3eef872a769754b0
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-12 12:24:29 +0100

    create methods for printing student names

--- directory.rb
+++ directory.rb
@@ -10,11 +10,22 @@ students = [
 	"The Joker",
 	"Joffrey Baratheon",
 	"Norman Bates"]
-	
-puts "The students of Villains Academy"
-puts "--------------"
-students.each do |student|
-	puts student
+
+def print_header
+	puts "The students of Villains Academy"
+	puts "-----------------"
 end
 
-print "Overall, we have #{students.count} great students"
+def print_names(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+	print "Overall, we have #{names.count} great students"	
+end
+
+print_header
+print_names(students)
+print_footer(students)
