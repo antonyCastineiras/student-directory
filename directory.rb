07bd5882f5a720f6415d55e9bf286339a78b8e9e
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-15 14:16:31 +0100

    refactor save and load methods so that the files don't need closing

--- directory.rb
+++ directory.rb
@@ -132,24 +132,24 @@ end
 #======================
 
 def load_students(filename="students.csv")
-	file = File.open(filename, "r")
-	file.readlines.each do |line|
-		name, cohort = line.chomp.split(',')
-		update_students(name,cohort)
+	File.open(filename, "r") do |file|
+		file.readlines.each do |line|
+			name, cohort = line.chomp.split(',')
+			update_students(name,cohort)
+		end
 	end
-	file.close
 end
 
 def save_students
 	puts "Save to which file?"
 	file_name = STDIN.gets.chomp
-	file = File.open(file_name, "w")
-	@students.each do |student|
-		student_data = [student[:name], student[:cohort]]
-		csv_line = student_data.join(",")
-		file.puts csv_line
+	File.open(file_name, "w") do |file|
+		@students.each do |student|
+			student_data = [student[:name], student[:cohort]]
+			csv_line = student_data.join(",")
+			file.puts csv_line
+		end
 	end
-	file.close
 end
 
 def set_first_argv
